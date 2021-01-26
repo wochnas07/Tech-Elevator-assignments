a67b2c5bd07e3d82ed33033a0749ef6002ccdf91
Frank Fella <frank@techelevator.com>
2021-01-26 05:37:05 -0500

    Instructor Push 01/26/2021 05:37:04

---
+++ module-1/12_Polymorphism/12-PlayingCard-Lecture-Frank/java/src/main/java/com/techelevator/UsePlayingCards.java
@@ -0,0 +1,52 @@
+package com.techelevator;
+
+public class UsePlayingCards {
+
+	public static void main(String[] args) {
+		
+		AmericanPlayingCard aUSACard  = new AmericanPlayingCard(1, "HEARTS");
+		System.out.println("aUSACard is :");
+		aUSACard.showCard();
+
+		AmericanPlayingCard aUSACard2 = new AmericanPlayingCard(14, "SPADES");
+		System.out.println("aUSACard2 is :" );
+		aUSACard2.showCard();
+		
+		AmericanPlayingCard aUSACard3 = new AmericanPlayingCard(-1, "SPADES");
+		System.out.println("aUSACard3 is :");
+		aUSACard3.showCard();
+			
+		AmericanPlayingCard aUSACard4 = new AmericanPlayingCard(11, "JASON");
+		System.out.println("aUSACard4 is :");
+		aUSACard4.showCard();
+		
+		ItalianPlayingCard anItalianCard1 = new ItalianPlayingCard(13, "SWORDS");
+		System.out.println("anItalianCard1 is :");
+		anItalianCard1.showCard();
+		
+		ItalianPlayingCard anItalianCard2 = new ItalianPlayingCard(9, "COINS");
+		System.out.println("anItalianCard2 is :");
+		anItalianCard2.showCard();
+		
+		ItalianPlayingCard anItalianCard3 = new ItalianPlayingCard(11, "JASON");
+		System.out.println("anItalianCard3 is :");
+		anItalianCard3.showCard();
+		
+		SwissPlayingCard aSwissCard1 = new SwissPlayingCard(13, "ROSES");
+		System.out.println("aSwissCard1 is :");
+		aSwissCard1.showCard();
+		
+		SwissPlayingCard aSwissCard2 = new SwissPlayingCard(9, "SHIELDS");
+		System.out.println("aSwissCard2 is :");
+		aSwissCard2.showCard();
+			
+		SwissPlayingCard aSwissCard3 = new SwissPlayingCard(10, "ACORNS");
+		System.out.println("anSwissCard3 is :");
+		aSwissCard3.showCard();
+			
+		SwissPlayingCard aSwissCard4 = new SwissPlayingCard(11, "JASON");
+		System.out.println("anSwissCard4 is :");
+		aSwissCard4.showCard();		
+	}
+
+}
