36056fefd2333ea1043cdad7d79060fcf42ac2b9
Frank Fella <frank@techelevator.com>
2021-01-26 06:02:22 -0500

    Instructor Push 01/26/2021 06:02:22

---
+++ module-1/12_Polymorphism/student-exercise/java/src/main/java/com/techelevator/BankCustomer.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * BankCustomer
+ */
+public class BankCustomer {
+
+    private List<Accountable> accounts = new ArrayList<>();   // List of Accountable objects
+    
+}
---
+++ module-1/12_Polymorphism/student-exercise/java/src/main/java/com/techelevator/CreditCardAccount.java
@@ -0,0 +1,23 @@
+package com.techelevator;
+
+/**
+ * CreditCardAccount
+ */
+public class CreditCardAccount implements Accountable {
+	
+/*******************************************************************
+ * Place the code for  your CreditCard class here
+ *******************************************************************/
+
+
+    public int getBalance() {
+    	//***************************************************************
+    	// This should return the negative of the debt data member
+    	//***************************************************************
+    	
+        return -0;
+    }
+
+ 
+
+}
