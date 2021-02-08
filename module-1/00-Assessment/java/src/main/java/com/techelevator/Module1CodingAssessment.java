5ee4c1d02e299163cab8031962acdea6d1778031
Samantha Wochna <samanthawochna@gmail.com>
2021-02-08 10:19:26 -0500

    teller machine

--- module-1/00-Assessment/java/src/main/java/com/techelevator/Module1CodingAssessment.java
+++ module-1/00-Assessment/java/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 // This is the application program for the assessment
 
 public class Module1CodingAssessment {
@@ -9,6 +11,22 @@ public class Module1CodingAssessment {
 		System.out.println("Module 1 Coding Assessment\n");
 		
 		// Put the code to test your assessment classes here
-	}
+		
+		boolean isValid(String numbersEntered) {
+			Scanner numebersEntered = new Scanner(System.in);
+			
+			System.out.println("Enter card numeber: ");
+			String cardNumber = numbersEntered.nextLine();
+			
+			
+			if (cardNumber.startsWith("5") && cardNumber.length()==16) {
+				return true;
+			} if (cardNumber.startsWith("4") && (cardNumber.length()==13 || cardNumber.length()==16)) {
+				return true;
+			}
+
+		}
+	} // I don't know why I'm so lost right now...
+		// I'm sorry I swear I know this stuff
 
 }
---
+++ module-1/00-Assessment/java/src/main/java/com/techelevator/TellerMachine.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class TellerMachine {
+	
+	private String manufacturer;
+	private double deposits;
+	private double withdrawals;
+	private double balance;
+	
+	public TellerMachine(String manufacturer, double deposits, double withdrawals) {
+		this.manufacturer = manufacturer;
+		this.deposits = deposits;
+		this.withdrawals = withdrawals;
+		
+	}
+}
