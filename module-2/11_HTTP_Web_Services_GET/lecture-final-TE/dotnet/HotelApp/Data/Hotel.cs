c3134db11de59ecaf7cb5eedabcd36d3bbede5ff
Frank Fella <frank@techelevator.com>
2021-02-22 15:27:53 -0500

    Instructor Push 02/22/2021 15:27:53

---
+++ module-2/11_HTTP_Web_Services_GET/lecture-final-TE/dotnet/HotelApp/Data/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTTP_Web_Services_GET_lecture
+{
+    class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public int RoomsAvailable { get; set; }
+        public string CoverImage { get; set; }
+    }
+}
---
+++ module-2/11_HTTP_Web_Services_GET/lecture-final-TE/dotnet/HotelApp/Data/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTTP_Web_Services_GET_lecture
+{
+    class Review
+    {
+        public int HotelID { get; set; }
+        public string Title { get; set; }
+        public string ReviewText { get; set; }
+        public string Author { get; set; }
+        public int Stars { get; set; }
+    }
+}
