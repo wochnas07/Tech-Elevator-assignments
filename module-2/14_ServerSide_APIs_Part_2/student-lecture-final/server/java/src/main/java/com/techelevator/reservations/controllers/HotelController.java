ab3e9cd5146cc57ecb053f5b848315c58586c617
Frank Fella <frank@techelevator.com>
2021-02-25 15:11:38 -0500

    Instructor Push 02/25/2021 15:11:38

---
+++ module-2/14_ServerSide_APIs_Part_2/student-lecture-final/server/java/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -0,0 +1,134 @@
+package com.techelevator.reservations.controllers;
+
+import com.techelevator.reservations.dao.HotelDAO;
+import com.techelevator.reservations.dao.ReservationDAO;
+import com.techelevator.reservations.exception.HotelNotFoundException;
+import com.techelevator.reservations.exception.ReservationNotFoundException;
+import com.techelevator.reservations.models.Hotel;
+import com.techelevator.reservations.models.Reservation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HotelController {
+
+    private HotelDAO hotelDAO;              // Reference to the HotelDAO so we can use it's methods
+    private ReservationDAO reservationDAO;  // Reference to the ReservationDAO so we can use it's methods
+
+    // Send to the ctor of this class a HotelDAO and a Reservations DAO
+    // Spring MVC dependency injection automatically instantiates a HotelDAO object
+    //                       and ReservationDAO object abd passes them to the ctor
+    //
+    // WE do not instantiate the DAO object used by the controller - Spring MVC does it
+    //
+    // You must include the @Component annotation in the DAOs for Spring MVC to dependency inject them
+    public HotelController(HotelDAO hotelDAO, ReservationDAO reservationDAO) {
+        this.hotelDAO       = hotelDAO;        // Assign the reference for the HotelDAO the DAO passed
+        this.reservationDAO = reservationDAO;  // Assign the reference for the ReservationDAO the DAO passed
+    }
+
+    /**
+     * Return All Hotels
+     *
+     * @return a list of all hotels in the system
+     */
+    @RequestMapping(path = "/hotels", method = RequestMethod.GET)
+    public List<Hotel> list() {
+        return hotelDAO.list();
+    }
+
+    /**
+     * Return hotel information
+     *
+     * @param id the id of the hotel
+     * @return all info for a given hotel
+     */
+    @RequestMapping(path = "/hotels/{id}", method = RequestMethod.GET)
+    public Hotel get(@PathVariable int id) {
+        return hotelDAO.get(id);
+    }
+
+    /**
+     * Returns all reservations in the system
+     *
+     * @return all reservations
+     */
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservations() {
+        return reservationDAO.findAll();
+    }
+
+    /**
+     * Get a reservation by its id
+     *
+     * @param id
+     * @return a single reservation
+     */
+    @RequestMapping(path = "reservations/{id}", method = RequestMethod.GET)
+    public Reservation getReservation(@PathVariable int id) throws ReservationNotFoundException {
+        return reservationDAO.get(id);
+    }
+
+    /**
+     * List of reservations by hotel
+     *
+     * @param hotelID
+     * @return all reservations for a given hotel
+     */
+    @RequestMapping(path = "/hotels/{id}/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservationsByHotel(@PathVariable("id") int hotelID) throws HotelNotFoundException {
+        return reservationDAO.findByHotel(hotelID);
+    }
+
+    /**
+     * Create a new reservation for a given hotel
+     *
+     * @param reservation
+     * @param hotelID
+     */
+    @ResponseStatus(HttpStatus.CREATED)   // Set the HTTP status when the controller successfully completes to CREATED status
+    @RequestMapping(path = "/hotels/{id}/reservations", method = RequestMethod.POST)
+    
+    // The @Valid annotation in the parameter list of the methods tells the server to apply any valid annotations present in any class
+    public Reservation addReservation(@Valid @RequestBody Reservation reservation, @PathVariable("id") int hotelID)
+            throws HotelNotFoundException {
+        return reservationDAO.create(reservation, hotelID);
+    }
+
+    /**
+     * /hotels/filter?state=oh&city=cleveland
+     *
+     * @param state the state to filter by
+     * @param city  the city to filter by
+     * @return a list of hotels that match the city & state
+     */
+    @RequestMapping(path = "/hotels/filter", method = RequestMethod.GET)
+    public List<Hotel> filterByStateAndCity(@RequestParam String state, @RequestParam(required = false) String city) {
+
+        List<Hotel> filteredHotels = new ArrayList<>();
+        List<Hotel> hotels = list();
+
+        // return hotels that match state
+        for (Hotel hotel : hotels) {
+
+            // if city was passed we don't care about the state filter
+            if (city != null) {
+                if (hotel.getAddress().getCity().toLowerCase().equals(city.toLowerCase())) {
+                    filteredHotels.add(hotel);
+                }
+            } else {
+                if (hotel.getAddress().getState().toLowerCase().equals(state.toLowerCase())) {
+                    filteredHotels.add(hotel);
+                }
+
+            }
+        }
+
+        return filteredHotels;
+    }
+
+}
---
+++ module-2/14_ServerSide_APIs_Part_2/student-lecture-final/server/java/src/main/java/com/techelevator/reservations/models/Reservation.java
@@ -0,0 +1,101 @@
+package com.techelevator.reservations.models;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class Reservation {
+
+	// We can use validation annotations to verify data is what expect
+	//    aka Bean Validation
+	// The controller processing objects of this class must be told to apply the validation criteria by adding @Valid to parameter
+	
+    @Min (value=1)             // make sure the minimum value in id is at least 1
+	private int id;
+   
+    @Min (value=1)             // make sure the minimum value of hotelId is at least 1
+    private int hotelID;
+    
+    @NotBlank
+    private String fullName;
+    
+    @NotNull                   // make sure checkinDate and checkoutDate is not null
+    private String checkinDate;
+    
+    @NotNull
+    private String checkoutDate;
+    
+    @Min(value=1, message="Must have at least one guest for a reservations")
+    @Max(value=4, message="Sorry, no more than 4 guests in a room")
+    private int guests;
+
+    public Reservation() {
+
+    }
+
+    public Reservation(int id, int hotelID, String fullName, String checkinDate, String checkoutDate, int guests) {
+        this.id = id;
+        this.hotelID = hotelID;
+        this.fullName = fullName;
+        this.checkinDate = checkinDate;
+        this.checkoutDate = checkoutDate;
+        this.guests = guests;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHotelID() {
+        return hotelID;
+    }
+
+    public void setHotelID(int hotelID) {
+        this.hotelID = hotelID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCheckinDate() {
+        return checkinDate;
+    }
+
+    public void setCheckinDate(String checkinDate) {
+        this.checkinDate = checkinDate;
+    }
+
+    public String getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(String checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public int getGuests() {
+        return guests;
+    }
+
+    public void setGuests(int guests) {
+        this.guests = guests;
+    }
+
+    @Override
+    public String toString() {
+        return "\n--------------------------------------------" + "\n Reservation Details"
+                + "\n--------------------------------------------" + "\n Id: " + id + "\n Hotel Id: " + hotelID
+                + "\n Full Name: " + fullName + "\n Checkin Date: " + checkinDate + "\n Checkout Date: " + checkoutDate
+                + "\n Guests: " + guests;
+    }
+}
